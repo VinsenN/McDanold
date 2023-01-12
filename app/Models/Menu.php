75ce2249c0897e3921476d0a74482165dd7a344f
Vio Albert Ferdinand <vioalbert08@gmail.com>
2023-01-12 14:38:19 +0700

    Add models and eloquent relationships

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -14,4 +14,19 @@ class Menu extends Model
     public $timestamps = false;
 
     protected $guarded = [];
+
+    public function category()
+    {
+        return $this->hasOne(Category::class, 'id', 'category_id');
+    }
+
+    public function orderDetail()
+    {
+        return $this->belongsTo(OrderDetail::class);
+    }
+
+    public function transactionDetail()
+    {
+        return $this->belongsTo(OrderDetail::class);
+    }
 }
