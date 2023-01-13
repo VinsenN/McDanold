e5b8bb313801517ed949f51c6ac49af2dcd6cf4f
Gregorius Emmanuel Henry <emmanuelhenry2002@gmail.com>
2023-01-13 22:06:03 +0700

    Testing modal-based success message

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,14 +2,31 @@
 
 @section('content')
     @if (session()->has('success'))
-        <div class="toast align-items-center show my-0 mx-auto mt-3" role="alert" aria-live="assertive" aria-atomic="true">
+        <!-- <button data-bs-toggle="modal" data-bs-target="#messageModal">testing kata-kata</button> -->
+        <script>
+            window.onload = () => {
+                document.querySelector('[data-bs-target="#messageModal"]');
+            }
+        </script>
+
+        <div class="modal fade" id="messageModal" tabindex="-1" aria-labelledby="messageModalLabel"
+                    aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <span class="fw-bold">{{ session()->get('success') }}</span>
+                </div>
+                <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+        </div>
+
+        <!-- <div class="toast align-items-center show my-0 mx-auto mt-3" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
                 <div class="toast-body text-success fw-bold">
                     {{ session()->get('success') }}
                 </div>
                 <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-        </div>
+        </div> -->
     @endif
 
     <main class="container pt-0 pb-5">
