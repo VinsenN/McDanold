98678554c1e812efca6897b7d244b07b1fcfb81b
Vinsen Nawir <vinsennawir@gmail.com>
2023-01-15 22:40:04 +0700

    Update addProduct.blade.php

--- resources/views/menu/manage/addProduct.blade.php
+++ resources/views/menu/manage/addProduct.blade.php
@@ -9,12 +9,20 @@
                 Add Menu Form
             </div>
             <div class="mb-3">
-                <label for="nameInput" class="form-label fw-semibold">Menu Name</label>
-                <input class="form-control" id="nameInput">
+                <label for="nameInput" class="form-label fw-semibold">Name</label>
+                <input class="form-control" id="nameInput" name="name">
+            </div>
+            <div class="mb-3">
+                <label for="categoryInput" class="form-label fw-semibold">Category</label>
+                <select class="form-select" id="categoryInput">
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
             </div>
             <div class="mb-3">
                 <label for="descriptionInput" class="form-label fw-semibold">Description</label>
-                <textarea class="form-control" id="descriptionInput" rows="3"></textarea>
+                <textarea class="form-control" id="descriptionInput" rows="3" name="description"></textarea>
             </div>
             <div class="mb-3">
                 <label for="priceInput" class="form-label fw-semibold">Price</label>
@@ -22,7 +30,7 @@
                     <div class="row align-items-center">
                         <div class="col-6 input-group">
                             <span class="input-group-text" id="basic-addon1">IDR</span>
-                            <input class="form-control" id="priceInput">
+                            <input class="form-control" id="priceInput" name="price">
                         </div>
                     </div>
                 </div>
@@ -31,11 +39,17 @@
                 <label for="imageInput" class="form-label fw-semibold">Image</label>
                 <div class="row align-items-center">
                     <div class="col-6">
-                        <input type="file" class="form-control" id="imageInput">
+                        <input type="file" class="form-control" id="imageInput" name="photo">
                     </div>
                 </div>
             </div>
-            <button type="submit" class="btn btn-success float-end mb-4 px-4">Add Menu</button>
+            <div class="mb-3">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" name="is_recommended">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Recommended
+                </label>
+            </div>
+            <button type="submit" class="btn btn-success mb-4 px-4">Add Menu</button>
         </form>
     </div>
 @endsection
